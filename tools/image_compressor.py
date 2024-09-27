2308a508b9fb65e0dd7a7d8f2e68725cf46ba9cb
Larry L <xinli071@outlook.com>
2024-09-27 16:14:39 +0800

    tools: add some info message

--- tools/image_compressor.py
+++ tools/image_compressor.py
@@ -22,9 +22,13 @@ class ImageCompressor:
             print(Error(f'Invalid source format, please choose a valid source!'))
 
         if self.tool == 'opencv':
+            print(Info('Using OpenCV | 使用 OpenCV'))
             self._opencv_compressor(src, quality)
         elif self.tool == 'pillow':
+            print(Info('Using PIL | 使用 PIL'))
             self._pillow_compressor(src, quality)
+
+        print(Info('Finished! | 已完成！'))
 
     def _opencv_compressor(self, src: Path, quality):
         import cv2
@@ -32,13 +36,14 @@ class ImageCompressor:
             images = list(src.rglob('*.jpg'))
             progress = Progress()
             count = 0
+            print(Info(f'Compressing... | 正在压缩...'))
             for jpg in images:
                 count += 1
                 progress.progress_bar(current=count, total=len(images))
                 cv2.imwrite(jpg, cv2.imread(jpg), [int(cv2.IMWRITE_JPEG_QUALITY), quality])
         else:
             img = cv2.imread(src)
-            print(Info('image passed'))
+            print(Info('Image passed'))
         pass
 
     def _pillow_compressor(self, src: Path, quality):
